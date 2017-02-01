47c314b9e552df4d6a53cb797693181e2eeae726
Mike Simmonds <mike@simmo.me>
2017-02-01 19:29:03 +0000

    Updating to latest react router

--- client/components/error.js
+++ client/components/error.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import { Motion, spring, presets } from 'react-motion'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 
 import 'styles/components/error'
 
--- client/components/module.js
+++ client/components/module.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 import Icon from 'components/icon'
 
 import 'styles/components/module'
